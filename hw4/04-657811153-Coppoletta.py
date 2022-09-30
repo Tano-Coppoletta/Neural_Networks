6dee194d53d1951b76c485caa7c8c067be590ddb
Gaetano-Coppoletta <tano.coppoletta@gmail.com>
2022-09-29 19:34:59 -0500

    HW4 | finished + report

--- hw4/04-657811153-Coppoletta.py
+++ hw4/04-657811153-Coppoletta.py
@@ -1,67 +1,92 @@
-from ast import Num
 import numpy as np
 import matplotlib.pyplot as plt
-import gzip
-import random
 
 def identity_function(x):
     return x
 
-def identity_derivative(x):
-    return 1
-
 def mean_squared_error(d,f):
-    return ((d-f)**2).sum() / d.size
+    return np.sum((d-f)**2)/300 
 
+def tanh_derivative(x):
+    return 1- np.tanh(x)**2
+    
 if __name__ == '__main__':
-    np.random.seed(1)
+    np.random.seed(234)
     n = 300
     x = np.random.uniform(0,1,(n))
     v = np.random.uniform(-1/10,1/10,(n))
-
+    N=24
     d = np.sin(20*x)+3*x+v
 
     plt.title("Plot (xi,di)")
 
     plt.xlabel("xi")
     plt.ylabel("di")
-  #  plt.ylim(-1.15,1.15)
     plt.grid()
     plt.scatter(x,d,color='green',marker='^',s=15)
     plt.show()
 
-    input_size=1
-    hidden_layer=24
-    output_size=1
+    eta=0.01
 
     
 
-    W1 = np.random.uniform(-1,1,(input_size,hidden_layer))
-    W2 = np.random.uniform(-1,1,(hidden_layer,output_size))
-    #25 bias, the first 24 are for the first layer
-    bias = np.random.uniform(-1,1,(1,hidden_layer+output_size))
+    W1 = np.random.randn(N,2) # weights + bias vector
+    #col 0 = bias
+    #col 1 = weights
+    W2 = np.random.randn(1,N+1) # last bias + weights
+    # | b | w0 | w1 | ... |w_N-1 |
 
-    for i in range(n):
-        #feedforward 
-        A=np.dot(x[i],W1)
-        Ab=np.sum(A,bias[:hidden_layer])
-        A_activated= np.tanh(Ab)
+    epochs=10000
+    epoch_vs_mse = np.zeros((2,epochs))
+    outputs = np.zeros(n)
+    index=0
+    epoch=0
 
-        B=np.dot(A_activated,W2)
-        Bb=np.sum(B,bias[-1])
-        output= identity_function(Bb)
+    while(epoch!=epochs):
+        epoch_vs_mse[0,epoch]=epoch
+        for i in range(n):
+            #induced local field 1
+            vl1 = W1 @ [[1],[x[i]]] 
+            y1 = np.tanh(vl1)   
+            #induced local field 2
+            y2 = np.append([1],y1)
+            vl2 = np.dot(W2,y2)   
+            outputs[index]=identity_function(vl2)
+           
+            #backpropagation
+            deltaL = d[i]-outputs[index]
+            delta1 = np.multiply((np.dot(np.transpose(W2), deltaL.reshape(1,1)))[1:,:],tanh_derivative(vl1))
+            index+=1
 
-        #feedback
-        #derivative of tanh = 1-tanh^2
-        O = d[i] - output
-        C = identity_derivative(O)
+            #update the weights
+            W1 = W1 + (eta * np.dot(delta1, [[1,x[i]]]))
+            W2 = W2 + (eta* np.dot(deltaL, y2))
 
-        xw = np.dot(x[i],W1) 
-        xwb = xw + bias[:hidden_layer]
-        D = 1- np.tanh(xwb)**2 
+        
+        epoch_vs_mse[1,epoch] = mean_squared_error(d,outputs)
+       # if(epoch != 0 and epoch_vs_mse[1,epoch]>epoch_vs_mse[1,epoch-1]):
+       #     eta=0.9*eta
+        print(epoch,epoch_vs_mse[1,epoch])
+        if(epoch_vs_mse[1,epoch]<0.01):
+            print("End at epoch",epoch)
+            break
+        epoch+=1
+        index=0
+    #plot
+    plt.title("Epoch vs MSE")
+    plt.xlabel("Epoch")
+    plt.ylabel("MSE")
+    plt.plot(epoch_vs_mse[0,1:epoch],epoch_vs_mse[1,1:epoch],'r')
+    plt.show()
 
-       # W1 = W1 - eta * 
+    xs, ys = zip(*sorted(zip(x, outputs)))
+    plt.xlabel("xi")
+    plt.ylabel("di")
+    plt.grid()
+    plt.scatter(x,d,color='green',marker='^',s=15)
+    plt.plot(xs,ys,'r')
+    plt.show()
 
 
-    eta=1
+
 
